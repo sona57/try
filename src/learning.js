ba1ae4db2aba4b698f989c5d79e67e887437e52b
Sonam Hemnani <hemnani.sonam@gmail.com>
2019-05-12 21:13:52 +0530

    Added my project

---
+++ src/learning.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Learning extends React.Component{
+    
+ render(){
+     
+     return <div> {this.props.son} Intelligent Sonam</div>
+ }   
+    
+    
+}
+
+export default Learning
